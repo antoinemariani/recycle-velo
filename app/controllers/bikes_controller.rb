e206050406b4ab26eea0f269113349e8ff64c789
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2022-11-29 12:02:07 +0100

    Bikes controller & view show basique OK

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,42 @@
 class BikesController < ApplicationController
+  before_action :set_params, only: %I[show edit update destroy]
+
+  def index
+    @bikes = Bike.all
+  end
+
+  def show; end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    # @bike.user = current_user
+    @bike.save!
+    redirect_to bike_path(@bike)
+  end
+
+  def edit; end
+
+  def update
+    @bike.update!(bike_params)
+    redirect_to bike_path(@bike)
+  end
+
+  def destroy
+    @bike.destroy!
+    redirect_to bikes_path, status: :see_other
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:name, :model, :brand)
+  end
+
+  def set_params
+    @bike = Bike.find(params[:id])
+  end
 end
