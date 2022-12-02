009bdce058ce30f9a29eb3c40e84e59e685a8730
Antoine Mariani <antoine.mariani@me.com>
2022-12-02 15:43:17 +0100

    upgrading bike diag (show) & result page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
+
+import PopoverController from "./popover_controller"
+application.register("popover", PopoverController)
