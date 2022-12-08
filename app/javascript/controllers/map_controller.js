69834caeb9633430cafeafefc4c7204a2590f822
Antoine Mariani <antoine.mariani@me.com>
2022-12-08 11:12:38 +0100

    GGMaps from Vanilla to Stimulus = hard refresh bug is fixed

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,18 +1,26 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
+import GMaps from 'gmaps/gmaps.js';
 
-// Connects to data-controller="map"
-// export default class extends Controller {
-//   static values = {
-//     apiKey: String,
-//     markers: Array
-//   }
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
 
-  // connect() {
-  //   mapboxgl.accessToken = this.apiKeyValue
-
-  //   this.map = new mapboxgl.Map({
-  //     container: this.element,
-  //     style: "mapbox://styles/mapbox/streets-v10"
-  //   })
-  // }
-// }
+  connect() {
+    // const mapElement = document.getElementById('map');// don't try to build a map if there's no div#map to inject in
+    console.log("map connected")
+    console.log("salut")
+    const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+    const markers = JSON.parse(this.element.dataset.markers);
+    map.addMarkers(markers);
+    if (markers.length === 0) {
+      map.setZoom(2);
+    } else if (markers.length === 1) {
+      map.setCenter(markers[0].lat, markers[0].lng);
+      map.setZoom(14);
+    } else {
+      map.fitLatLngBounds(markers);
+    }
+  }
+}
